3af775a59e265bc0285a48c15668b491cc66a440
Tumiso Graig Ramaboya <tumishosono6@gmail.com>
2025-05-18 15:18:34 -0700

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,7 +10,8 @@ SELECT
   u.first_name,
   u.last_name,
   u.email
-
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id;
 
 -- 2. LEFT JOIN: Retrieve all properties with their reviews (if any)
 SELECT
@@ -19,13 +20,23 @@ SELECT
   r.review_id,
   r.rating,
   r.comment
-
+FROM properties p
+LEFT JOIN reviews r ON p.property_id = r.property_id;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings
--- PostgreSQL version:
+-- PostgreSQL version
 SELECT
   u.user_id,
   u.first_name,
   b.booking_id,
   b.start_date,
   b.end_date
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id;
+
+-- MySQL workaround using UNION
+-- SELECT u.user_id, u.first_name, b.booking_id, b.start_date, b.end_date
+-- FROM users u LEFT JOIN bookings b ON u.user_id = b.user_id
+-- UNION
+-- SELECT u.user_id, u.first_name, b.booking_id, b.start_date, b.end_date
+-- FROM bookings b LEFT JOIN users u ON b.user_id = u.user_id;
