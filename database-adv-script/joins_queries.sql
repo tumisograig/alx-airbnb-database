a9245bd02e521c8a359980afd2fa94792aee1ebc
Tumiso Graig Ramaboya <tumishosono6@gmail.com>
2025-05-18 15:14:43 -0700

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,31 @@
+-- Task 0: Complex Queries with Joins
 
+-- 1. INNER JOIN: Retrieve all bookings with user details
+SELECT
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.total_price,
+  u.user_id,
+  u.first_name,
+  u.last_name,
+  u.email
+
+
+-- 2. LEFT JOIN: Retrieve all properties with their reviews (if any)
+SELECT
+  p.property_id,
+  p.name,
+  r.review_id,
+  r.rating,
+  r.comment
+
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings
+-- PostgreSQL version:
+SELECT
+  u.user_id,
+  u.first_name,
+  b.booking_id,
+  b.start_date,
+  b.end_date
